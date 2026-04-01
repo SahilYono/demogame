d510946b59c7c76a055bce31516c7d81da0b635f
SahilYono <sahilvr66@gmail.com>
2026-04-02 00:02:42 +0530

    add

--- WarShip3D/Assets/SCRIPT/enemy.cs
+++ WarShip3D/Assets/SCRIPT/enemy.cs
@@ -26,7 +26,7 @@ public class enemy : MonoBehaviour
 
     private void ProcessHit()
     {
-        //healthAmount--;nnnn
+        //healthAmount--------;
        /* if (healthAmount <= 0)
         {
             Instantiate(DestroVFX, transform.position, Quaternion.identity);
